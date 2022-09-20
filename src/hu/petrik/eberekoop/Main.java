2389d00005d6bce7ef3112d1e5a50cee1d0e0d7b
Bodor István <istvanbodor19@gmail.com>
2022-09-20 13:57:10 +0200

    Ember osztály - adattagok - paraméteres konstruktor - Tostring
    Main osztály ember felvétele és kiiratása

--- src/hu/petrik/eberekoop/Main.java
+++ src/hu/petrik/eberekoop/Main.java
@@ -2,7 +2,14 @@ package hu.petrik.eberekoop;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+
+        Ember human = new Ember("Gipsz Jakab", "2000-01-01","Budapest");
+        System.out.println(human);
+
+
+
+
     }
 
 
